f489a142e345b69191893cd887725d2d49befedc
Nisarg-jain <jainnisag98@gmail.com>
2026-06-16 19:50:57 +0530

    Initial commit for Redux Toolkit setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-500 text-white">Welcome to Vite</h1>
+    </>
+  )
+}
+
+export default App
